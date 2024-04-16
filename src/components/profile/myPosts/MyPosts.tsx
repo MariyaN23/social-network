0c7e27458fac7b5e6b50d47046cd97fae03286af
MariyaN23 <sellaite505@gmail.com>
2024-04-16 21:14:41 +0300

    87

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -4,37 +4,16 @@ import {Post} from './post/Post';
 import {MyPostsPropsType} from './MyPostsContainer';
 import {PostForm} from './PostForm';
 
-export const MyPosts = (props: MyPostsPropsType) => {
+export const MyPosts = React.memo((props: MyPostsPropsType)=> {
     const postsElements =
-    props.posts.map(p=> <Post key={p.id} message={p.message} likeCounts={p.likeCounts}/>)
-
-    /*const newPostElement = React.createRef<HTMLTextAreaElement>()
-
-    const addPostHandler =()=> {
-        props.addPost()
-    }
-
-    const changeNewPostTextHandler = (e: ChangeEvent<HTMLTextAreaElement>)=> {
-        props.updateNewPostText(e.currentTarget.value)
-    }*/
-
+        props.posts.map(p => <Post key={p.id} message={p.message} likeCounts={p.likeCounts}/>)
     return (
         <div className={s.postsBlock}>
             <h3>My Posts</h3>
-            {/*<div>
-                <div className={s.textarea}>
-                    <textarea value={props.newPostText}
-                              onChange={changeNewPostTextHandler}
-                              placeholder={'Your news...'} ref={newPostElement} />
-                </div>
-                <div className={s.buttonPost}>
-                    <button onClick={addPostHandler}>Add post</button>
-                </div>
-            </div>*/}
-            <PostForm sendPost={props.addPost} />
+            <PostForm sendPost={props.addPost}/>
             <div>
                 {postsElements}
             </div>
         </div>
     );
-};
+})

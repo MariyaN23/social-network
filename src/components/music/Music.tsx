8b523770f9ca8efc4ca39a1c160793a9d7baa4ed
MariyaN23 <sellaite505@gmail.com>
2024-08-06 20:11:46 +0300

    music

--- src/components/music/Music.tsx
+++ src/components/music/Music.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import s from "./Music.module.css";
 
 export const Music = () => {
     return (
-        <div>
-            Music
-        </div>
+        <>
+            <div className={s.pageName}>Music</div>
+            <div className={s.musicWrapper}>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/hMG8zKpc-J4"
+                        allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            </div>
+        </>
     );
 };
